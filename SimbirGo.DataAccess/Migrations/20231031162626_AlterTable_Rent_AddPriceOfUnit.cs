2cb678d9e6f923f7045f4f3043764ad3d4f55685
ruslan.m <ruslan.m@ati.su>
2023-10-31 19:51:27 +0300

    Migrations

---
+++ SimbirGo.DataAccess/Migrations/20231031162626_AlterTable_Rent_AddPriceOfUnit.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Simbir.Go.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTable_Rent_AddPriceOfUnit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "PriceOfUnit",
+                table: "Rents",
+                type: "double precision",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PriceOfUnit",
+                table: "Rents");
+        }
+    }
+}
